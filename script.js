34078287e2e2d074f3902e2587b57258503072eb
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-09 20:09:24 +0200

    project finished

--- script.js
+++ script.js
@@ -3,37 +3,60 @@ const userNameInput = document.querySelector('#username')
 const passwordInput = document.querySelector('#password')
 const passwordConfirmInput = document.querySelector('#password-confirmation')
 const termsInput = document.querySelector('#terms')
+const errorsContainer = document.querySelector('.errors')
+const errorList = document.querySelector('.errors-list')
 
-//console.log('test')
 
-
-// TODO: Create an event listener for when the form is submitted and do the following inside of it.
 form.addEventListener('submit', e =>
 {
-    e.preventDefault()
     const errorMessages = []
-    //validations:
     if (userNameInput.value.length < 6)
     {
         errorMessages.push('Username must be at least 6 characters')
     }
-    
 
-    console.log(errorMessages)
+    if (passwordInput.value.length < 10)
+    {
+        errorMessages.push('Password should have at least 6 characters')
+    }
+
+    if (passwordConfirmInput.value !== passwordInput.value)
+    {
+        errorMessages.push('Password must match')
+    }
+
+    if (!termsInput.checked)
+    {
+        errorMessages.push('You must accept the terms')
+    }
+
+    if (errorMessages.length > 0)
+    {
+        e.preventDefault()
+        showErrors(errorMessages)
+    }
+
 })
 
 
-
-// TODO: Define this function
 function clearErrors()
 {
-
+    while (errorList.children[0] != null)
+    {
+        errorList.removeChild(errorList.children[0])
+    }
+    errorsContainer.classList.remove('show')
 }
 
-// TODO: Define this function
 function showErrors(errorMessages)
 {
-    // Add each error to the error-list element
-    // Make sure to use an li as the element for each error
-    // Also, make sure you add the show class to the errors container
+    clearErrors()
+    errorMessages.forEach(mess =>
+    {
+        const li = document.createElement('li')
+        li.innerText = mess
+        errorList.appendChild(li)
+    })
+
+    errorsContainer.classList.add('show')
 }
